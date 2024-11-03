8f5452c58b7a1dc1d8fa8bffbd5f7f68dd692c7f
Yash Nasery <naseryyash@gmail.com>
2024-11-03 11:08:17 +0530

    Minor code cleanup

--- src/main/java/distributed/systems/Main.java
+++ src/main/java/distributed/systems/Main.java
@@ -10,21 +10,12 @@ import java.io.IOException;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Leader election part
         LeaderElection leaderElection = new LeaderElection();
         try {
             leaderElection.leaderElectionAlgorithmImpl();
         } catch (IOException | InterruptedException | KeeperException e) {
             throw new RuntimeException(e);
         }
-
-        // Watcher for various events part
-//        ZookeeperWatcher zookeeperWatcher = new ZookeeperWatcher();
-//        try {
-//            zookeeperWatcher.watcherImpl();
-//        } catch (IOException | InterruptedException | KeeperException e) {
-//            throw new RuntimeException(e);
-//        }
 
     }
 }
